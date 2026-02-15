1c71c35f6c8a18b91abf99b48ca44277ec7103c2
leeladhar <sanaleeladhar2000@gmail.com>
2026-02-15 19:51:00 +0530

    action

---
+++ 08-arreys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Fruits = ("apple","mango","jama")
+
+echo "Fruits are: ${Fruits[@]}"
+echo "first Frust is: ${Fruits[0]}"
+echo "second Fruit is: ${Fruits[1]}"
+echo "third Fruit name is: ${Fruits[2]}"
