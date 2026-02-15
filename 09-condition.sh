2073a9ac008157597d7b0616daaf3f8c7d78f4bd
leeladhar <sanaleeladhar2000@gmail.com>
2026-02-15 20:03:42 +0530

    action

---
+++ 09-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Number=$1
+
+# -gt = Greater than
+# -lt = less than
+# -eq = Equal
+# -ne = not Equal
+
+if [$Number -gt 20]; then
+    echo "Given number: $Number is grater than 20"
+fi
