868227630cf242954236284552fbe568bb3c9fe5
leeladhar <sanaleeladhar2000@gmail.com>
2026-02-15 17:11:09 +0530

    action

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+per1=suresh
+per2=Ramesh
+echo "$per1:: Hello $per2, how are you"
+echo "$per2:: Hi $per1, i am fine thanks for asking. how are you"
+echo "$per1:: doing fine, whar are you learning"
+echo "$per2:: i am learing shell, what about you?"
