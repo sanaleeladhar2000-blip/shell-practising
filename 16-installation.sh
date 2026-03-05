3a52b39eb4a174e5e55ed96b8dcefc1f2c23487f
leeladhar <sanaleeladhar2000@gmail.com>
2026-03-05 20:33:10 +0530

    action

--- 16-installation.sh
+++ 16-installation.sh
@@ -4,7 +4,11 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then 
     echo "Please run the script as root user"
+   
     
 else
     echo "You are running the script as root user"
 fi
+
+echo "Installing nginx for the frontend"
+dnf install nginx -y 
