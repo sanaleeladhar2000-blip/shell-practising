b3da7759caeee856eb6374f82044230e97788bb3
leeladhar <sanaleeladhar2000@gmail.com>
2026-03-05 20:20:19 +0530

    action

---
+++ 16-installation.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then 
+    echo "Please run the script as root user"
+    exit 1
+else
+    echo "You are running the script as root user"
+fi
