26d5aa77edc8eaa289316acf3f327800d20ef4a5
leeladhar <sanaleeladhar2000@gmail.com>
2026-03-10 18:20:54 +0530

    action

---
+++ 22.colour.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+echo -e "$R i am learning Linux$N $Y i am learning shell scripting$N $G i am learning DevOps$N $B i am learning terraform$N"
+
